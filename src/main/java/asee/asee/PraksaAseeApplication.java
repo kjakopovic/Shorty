97712f171df358a286230095275cb759c6703f36
dini777 <karlojakopov@gmail.com>
2024-05-20 10:52:24 +0200

    Project setup

---
+++ src/main/java/asee/asee/PraksaAseeApplication.java
@@ -0,0 +1,13 @@
+package asee.asee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraksaAseeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraksaAseeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asee/asee/PraksaAseeApplicationTests.java
@@ -0,0 +1,13 @@
+package asee.asee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraksaAseeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
