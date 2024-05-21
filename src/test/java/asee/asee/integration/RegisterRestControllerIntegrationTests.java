dca8b10533498389e53f5aac21934c508e278572
dini777 <karlojakopov@gmail.com>
2024-05-21 10:31:35 +0200

    Gotova testiranja i fixevi na endpointu

---
+++ src/test/java/asee/asee/integration/RegisterRestControllerIntegrationTests.java
@@ -0,0 +1,15 @@
+package asee.asee.integration;
+
+import asee.asee.administration.services.UserService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class RegisterEndpointTests {
+
+    @Autowired
+    private UserService userService;
+
+    // remaining class code
+}
