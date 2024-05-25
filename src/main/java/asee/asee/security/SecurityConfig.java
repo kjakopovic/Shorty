49f354c665b19cb20e3a15367ec1362fc57e8bdf
dini777 <karlojakopov@gmail.com>
2024-05-25 09:46:48 +0200

    Docker setupan, još uvijek postoje problemi sa onim securitijem na swaggeru, ne znam kako fixati to

--- src/main/java/asee/asee/security/SecurityConfig.java
+++ src/main/java/asee/asee/security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers("/administration/register").permitAll()
                         //TODO: development purposes
                         .requestMatchers("/swagger-ui/index.html").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults());
